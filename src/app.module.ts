799e89af4dea0f9eca041384edacbf5f8b4c72ed
denwermir <denwerorigin1@yandex.ru>
2025-03-07 10:04:21 +0300

    07.03.2025 http://http://[::1]:3000/

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,67 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
-@Module({
-  imports: [],
+/*
+import { Module } from '@nestjs/common';
+import { AppController } from './data/app.controller';
+import { AppService } from './data/app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';*/
+
+/*@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'static'),
+      serveRoot: '/',
+      exclude: ['/api*'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
+})
+export class AppModule {}
+*/
+/*
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'static'),
+      serveRoot: '/',
+      exclude: ['/api*'],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api/{*test}'],
+      serveStaticOptions: {
+        fallthrough: false,
+      },
+    }),
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}*/
+
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './data/app.controller';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api/{*test}'],
+      serveStaticOptions: {
+        fallthrough: false,
+      },
+    }),
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,34 @@ async function bootstrap() {
 }
 bootstrap();
 */
+/*
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  //const app = await NestFactory.create(AppModule);
+  //await app.listen(3000);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setBaseViewsDir(join(__dirname, '../views'));
+  
+  app.setViewEngine('pug');
+  await app.listen(5000);
+
+}
+bootstrap();
+*/
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
