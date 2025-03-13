900b5958bdaebf34186d32164165779fab0cb505
denwermir <denwerorigin1@yandex.ru>
2025-03-13 18:42:53 +0300

    Добавление регистрации, добавление js файла, потом я закоменчу все ts файлы...

---
+++ public/reg.js
@@ -0,0 +1,20 @@
+//Инициализация кнопок
+const CreateAccountButton = document.getElementById('CAB')
+CreateAccountButton.addEventListener('click', CreateUserButton)
+let AccountCreateMessage = "Аккаунт " + username + " успешно создан!"
+
+//Функция создания пользователя
+function CreateUserButton(){
+    let username = document.getElementById('username').value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    const data = { username, email, password };
+
+    //Отправка запроса для создания пользователя
+    fetch('/api/elevion',{
+        method: 'Post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    })
+}
+    
